5cb7cbbd62bbd4a0e7e859250b4e73172a5b1f92
AartiPundalik <aartipundalik68@gmail.com>
2023-02-22 01:21:40 +0530

    [Aarti] Add : UC1 - Finding Maximum Number

---
+++ Generics_Program/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Program
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=================\nChoose Number :\n=================" +
+         "\nEnter 1:Finding Maximum Numbers ");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    FindMaximum.FindIntMaxVal(3, 5, 1);
+                    break;
+            }
+        }
+    }
+}
