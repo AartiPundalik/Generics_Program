75053dcc1f0e543e8706c4208b96e7bfca545ef0
AartiPundalik <aartipundalik68@gmail.com>
2023-02-22 08:33:32 +0530

    [Aarti] UC1.2 -Finding Secong Max Int Value

--- TestCaseMax/UnitTest1.cs
+++ TestCaseMax/UnitTest1.cs
@@ -5,14 +5,22 @@ namespace TestCaseMax
     public class Tests
     {
         FindMaxGenric<int> genericMthodForInt = new FindMaxGenric<int>();
-        
-        
+
+
         [Test]
         public void GivenMaxFirstNum_WhenAnalysed_ShouldReturnFirstMax_Int()
         {
             int result = genericMthodForInt.GenricValue(20, 10, 17);
             Assert.AreEqual(20, result);
             //Assert.Pass();
+        }
+        [Test]
+        public void GivenMaxFirstNum_WhenAnalysed_ShouldReturnSecondMax_Int()
+        {
+            int result = genericMthodForInt.GenricValue(26, 30, 17);
+            Assert.AreEqual(30, result);
+            //Assert.Pass();
+
         }
     }
 }
